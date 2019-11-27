1ce847e0a10d598329f08961d270929130424fb5
wenyuan <liwenyuan.zju@gmail.com>
2019-11-27 11:23:49 -0800

    find stroma slides

---
+++ Datasets/ProstatePair/find_stroma_slides.py
@@ -0,0 +1,28 @@
+import numpy as np
+import skimage
+import os, sys
+import scipy.io
+root_dir = os.getcwd()
+def find_root_dir(filepath, basename):
+    if os.path.basename(filepath) == basename:
+        return filepath
+    else:
+        root_dir = find_root_dir(os.path.dirname(filepath), basename)
+        return root_dir
+
+root_dir = find_root_dir(root_dir, "HDImGeneration")
+sys.path.append(root_dir)
+from utils import *
+
+## specify the stroma slides mat file path
+maskfolder = "./masks"
+filenames = files_under_folder_with_suffix(maskfolder, "mat")
+
+stroma_slides = []
+
+for i in range(len(filenames)):
+    mask = scipy.io.loadmat(os.path.join(maskfolder, filenames[i]))['ATmask']
+    if np.all(mask == 0):
+        stroma_slides.append(mask)
+
+print(stroma_slides)
