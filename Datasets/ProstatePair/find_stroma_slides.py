deff82cba7ee4892db43227a2dc86cea751e2bf5
wenyuan <liwenyuan.zju@gmail.com>
2019-11-27 11:28:22 -0800

    find stroma slides

--- Datasets/ProstatePair/find_stroma_slides.py
+++ Datasets/ProstatePair/find_stroma_slides.py
@@ -15,7 +15,7 @@ sys.path.append(root_dir)
 from utils import *
 
 ## specify the stroma slides mat file path
-maskfolder = "./masks"
+maskfolder = "/data/wenyuan/Path_R_CNN/Data_Pre_Processing/masks_sementic_mod-1200by1200"
 filenames = files_under_folder_with_suffix(maskfolder, "mat")
 
 stroma_slides = []
