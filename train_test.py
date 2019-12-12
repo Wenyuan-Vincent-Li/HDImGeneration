ba2149e0a3cb729a23c1a6d1b43ff4c97bba69f8
wenyuan <liwenyuan.zju@gmail.com>
2019-12-12 14:46:49 -0800

    pixpixHD loss

--- train_test.py
+++ train_test.py
@@ -3,13 +3,13 @@ from Training.train import train
 from Training import functions
 
 opt = TrainOptions().parse()
-opt.name = 'prostate'
+opt.name = 'prostateHD'
 Gs = []
 Zs = []
 NoiseAmp = []
-reals = []
-# reals = [[64, 64], [128, 128], [192, 192], [256, 256], [320, 320], [384, 384], [448, 448], [512, 512]]
-reals = functions.create_reals_pyramid([opt.fineSize, opt.fineSize], reals, opt)
+# reals = []
+reals = [[64, 64], [128, 128], [192, 192], [256, 256], [320, 320], [384, 384], [448, 448], [512, 512]]
+# reals = functions.create_reals_pyramid([opt.fineSize, opt.fineSize], reals, opt)
 
 opt.alpha = 1 ## Not to use reconstruction loss
 train(opt, Gs, Zs, NoiseAmp, reals)
