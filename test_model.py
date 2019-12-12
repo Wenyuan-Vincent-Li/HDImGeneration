36ab38ad2613d5a471c833b5ecbf1aab99855371
wenyuan <liwenyuan.zju@gmail.com>
2019-12-12 14:43:56 -0800

    pixpixHD loss

--- test_model.py
+++ test_model.py
@@ -4,12 +4,12 @@ from options.train_options import TrainOptions
 from Models.pix2pixHD import *
 opt = TrainOptions().parse()
 netD, netG = init_models(opt)
-
-x = torch.rand(4, 3, 32, 32).to(opt.device)
-y = torch.rand(4, 3, 32, 32).to(opt.device)
-mask = torch.rand(4, 4, 32, 32).to(opt.device)
-# output = netG(x, y, mask)
-output = netD(x, mask)
+w = h = 64
+x = torch.rand(4, 3, w, h).to(opt.device)
+y = torch.rand(4, 3, w, h).to(opt.device)
+mask = torch.rand(4, 4, w, h).to(opt.device)
+output = netG(x, y, mask)
+# output = netD(x, mask)
 
 
 print(output.shape)
--- train_test.py
+++ train_test.py
@@ -8,8 +8,8 @@ Gs = []
 Zs = []
 NoiseAmp = []
 reals = []
+# reals = [[64, 64], [128, 128], [192, 192], [256, 256], [320, 320], [384, 384], [448, 448], [512, 512]]
 reals = functions.create_reals_pyramid([opt.fineSize, opt.fineSize], reals, opt)
-
 
 opt.alpha = 1 ## Not to use reconstruction loss
 train(opt, Gs, Zs, NoiseAmp, reals)
