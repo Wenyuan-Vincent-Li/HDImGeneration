37000c892510bbdedc6effead2b8edaf77d00658
wenyuan <liwenyuan.zju@gmail.com>
2019-12-13 18:02:16 -0800

    pixpixHD with reconstruction loss

--- train_test.py
+++ train_test.py
@@ -11,9 +11,9 @@ NoiseAmp = []
 reals = [[64, 64], [128, 128], [192, 192], [256, 256], [320, 320], [384, 384], [448, 448], [512, 512]]
 # reals = functions.create_reals_pyramid([opt.fineSize, opt.fineSize], reals, opt)
 
-opt.alpha = 1e-3
+opt.alpha = 0
 opt.niter = 200
-opt.scale_factor = 0.78
-opt.noise_amp = 1e-2
+opt.scale_factor = 0.79
+opt.noise_amp = 0.1
 
 train(opt, Gs, Zs, NoiseAmp, reals)
