87746420a1acd7b588533d7b081c3f7afea66945
wenyuan <liwenyuan.zju@gmail.com>
2019-12-16 21:14:18 -0800

    train from scratch

--- train_test.py
+++ train_test.py
@@ -9,12 +9,12 @@ Zs = []
 NoiseAmp = []
 # reals = []
 # reals = [[64, 64], [128, 128], [192, 192], [256, 256], [320, 320], [384, 384], [448, 448], [512, 512]]
-reals = [[512,512]]
+reals = [[256, 256], [512,512]]
 # reals = functions.create_reals_pyramid([opt.fineSize, opt.fineSize], reals, opt)
 
 opt.alpha = 0
 opt.niter = 200
-opt.scale_factor = 0.84
+opt.scale_factor = 0.85
 opt.noise_amp = 1e-5
 
 train(opt, Gs, Zs, NoiseAmp, reals)
