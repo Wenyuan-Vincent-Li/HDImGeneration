10583edb3b26ee89b501e57bd4ce633c6607cab1
wenyuan <liwenyuan.zju@gmail.com>
2019-12-16 18:32:27 -0800

    pixpixHD with reconstruction loss

--- train_continue.py
+++ train_continue.py
@@ -7,7 +7,7 @@ opt.alpha = 0
 opt.name = 'prostateHD'
 opt.scale_factor = 0.80
 opt.niter = 200
-opt.noise_amp = 1
+opt.noise_amp = 0.1
 Gs, Zs, reals, NoiseAmp = functions.load_trained_pyramid(opt)
 Gs = Gs[:1]
 
