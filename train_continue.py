96a38ebc534ce2898462ed116698dbed2e64e852
wenyuan <liwenyuan.zju@gmail.com>
2019-12-21 11:31:57 -0800

    train from scratch

--- train_continue.py
+++ train_continue.py
@@ -3,11 +3,12 @@ from Training.train import train
 from Training import functions
 
 opt = TrainOptions().parse()
-opt.alpha = 0.1
+opt.alpha = 1
 opt.name = 'prostateHD'
-opt.scale_factor = 0.87
+opt.scale_factor = 0.88
 opt.niter = 200
 opt.noise_amp = 1
 Gs, Zs, reals, NoiseAmp = functions.load_trained_pyramid(opt)
 
+reals = reals + [[1024, 1024]]
 train(opt, Gs, Zs, NoiseAmp, reals)
